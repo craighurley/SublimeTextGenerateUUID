4a75d993c9ac617c69bf898ab7701edb56a30645
Craig Hurley <craig@thehurley.com>
2013-05-14 13:36:41 +1200

    Added unique UUIDs at multi cursor

--- GenerateUuidFourCommand.py
+++ GenerateUuidFourCommand.py
@@ -5,13 +5,13 @@ import uuid
 
 class GenerateUuidFourCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # create and copy the UUID into the paste buffer
-        uuid4 = "uuid_" + str(uuid.uuid4())
-        sublime.set_clipboard(uuid4)
-
         # paste the UUID at each selection point
         for cursors in self.view.sel():
+            uuid4 = "uuid_" + str(uuid.uuid4())
             self.view.replace(edit, cursors, uuid4)
+
+        # copy the last UUID to the clipboard
+        sublime.set_clipboard(uuid4)
 
         # update the status message
         sublime.status_message("Generated UUID version 4")
