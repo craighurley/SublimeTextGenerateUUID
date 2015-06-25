d432f7456cf9754cf89ce54253b04b5d68d9a0c0
Craig Hurley <craig@thehurley.com>
2015-06-25 21:58:45 +1200

    Removed "uuid" prefix.

--- GenerateUuidFourCommand.py
+++ GenerateUuidFourCommand.py
@@ -7,7 +7,7 @@ class GenerateUuidFourCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # paste the UUID at each selection point
         for cursors in self.view.sel():
-            uuid4 = "uuid_" + str(uuid.uuid4())
+            uuid4 = str(uuid.uuid4())
             self.view.replace(edit, cursors, uuid4)
 
         # copy the last UUID to the clipboard
